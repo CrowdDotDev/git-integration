3b94eb9e649b87cdfeeaf48825ed345d6bd0b13a
Joan Reyero <joan@crowd.dev>
2023-05-12 12:00:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,3 +31,23 @@ cd ~/git-integration
 cp -f ../git-integration-environment/dotenv-prod .env
 
 echo "The dotenv-prod file has been copied as .env"
+
+echo "Setting up cron job"
+# Check if the cron job entry already exists
+if ! crontab -l | grep -q "/home/ubuntu/venv/cgit/bin/crowd-git-ingest"; then
+    # Save the current crontab into a temporary file
+    crontab -l > tmp-cron
+
+    # Append the new cron job entry
+    echo "0 * * * * /home/ubuntu/venv/cgit/bin/crowd-git-ingest >> /data/repos/log/cron.log 2>&1" >> tmp-cron
+
+    # Install the new crontab
+    crontab tmp-cron
+
+    # Remove the temporary file
+    rm tmp-cron
+
+    echo "Cron job added successfully."
+else
+    echo "Cron job already exists. Nothing to do."
+fi
