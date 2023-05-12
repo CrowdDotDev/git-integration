3cd2af506d88e156d1ecc564d2c327d847aa02de
Ubuntu <ubuntu@ip-192-169-5-47.us-west-2.compute.internal>
2023-05-12 10:28:02 +0000

    Added env manager in install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,23 @@ source ~/venv/cgit/bin/activate
 pip install --upgrade pip
 cd ~/git-integration ; pip install -e .
 
+# Move one directory up from the git-integration folder
+cd ~
+
+# Check if git-integration-environment folder exists
+if [ ! -d "git-integration-environment" ]; then 
+  git clone git@github.com:CrowdDotDev/git-integration-environment.git
+else
+  # Pull the latest changes from git-integration-environment repository if it exists
+  cd git-integration-environment
+  git pull origin main
+  cd ~
+fi
+
+# Move back into the git-integration folder
+cd ~/git-integration
+
+# Copy the dotenv-prod file as .env, replacing it if it already exists
+cp -f ../git-integration-environment/dotenv-prod .env
+
+echo "The dotenv-prod file has been copied as .env"
