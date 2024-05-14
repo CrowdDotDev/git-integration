dd9cdde98daeb47608a39aa2072070526b2c1e81
garrrikkotua <kotuaigor@gmail.com>
2024-05-14 15:32:56 +0300

    wip

--- crowdgit/server.py
+++ crowdgit/server.py
@@ -3,8 +3,9 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import os
 from crowdgit import LOCAL_DIR
-import subprocess
+import asyncio
 from dotenv import load_dotenv
+from crowdgit.repo import get_repo_name
 
 load_dotenv()
 
@@ -16,12 +17,18 @@ REPOS_DIR = os.environ.get("REPOS_DIR", DEFAULT_REPOS_DIR)
 
 
 @app.get("/")
-async def root():
+async def root(token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    if token.credentials != os.environ["AUTH_TOKEN"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return {"message": "Hello World"}
 
 
-@app.get("/stats/{owner}/{repo}")
-def repo_stats(owner: str, repo: str, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+@app.get("/stats")
+async def repo_stats(remote: str, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
     if token.credentials != os.environ["AUTH_TOKEN"]:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -29,17 +36,19 @@ def repo_stats(owner: str, repo: str, token: HTTPAuthorizationCredentials = Depe
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    dir_name = f"{owner}-{repo}"
+    dir_name = get_repo_name(remote)
     repo_dir = os.path.join(REPOS_DIR, dir_name)
 
     if not os.path.exists(repo_dir):
         raise HTTPException(status_code=404, detail="Repository not found")
 
-    # using subprocess to run the git command and get number of commits in the repo
-
     cmd = f"git -C {repo_dir} rev-list --count HEAD"
-    result = subprocess.run(cmd, shell=True, capture_output=True)
-    if result.returncode != 0:
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, _ = await process.communicate()
+
+    if process.returncode != 0:
         raise HTTPException(status_code=500, detail="Internal server error")
 
-    return {"repo": repo, "owner": owner, "num_commits": int(result.stdout)}
+    return {"remote": remote, "num_commits": int(stdout)}
