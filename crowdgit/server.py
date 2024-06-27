324c3a5fbb0beef6b4d05f9f5cf71aa440fa1663
garrrikkotua <kotuaigor@gmail.com>
2024-06-27 14:59:36 +0300

    get user by email

--- crowdgit/server.py
+++ crowdgit/server.py
@@ -55,3 +55,39 @@ async def repo_stats(remote: str, token: HTTPAuthorizationCredentials = Depends(
         raise HTTPException(status_code=500, detail=f"Internal server error")
 
     return {"remote": remote, "num_commits": int(stdout)}
+
+
+@app.get("/user-by-email")
+async def get_user_name(
+    email: str, remote: str, token: HTTPAuthorizationCredentials = Depends(auth_scheme)
+):
+    if token.credentials != os.environ["AUTH_TOKEN"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    dir_name = get_repo_name(remote)
+    repo_dir = os.path.join(REPOS_DIR, dir_name)
+
+    if not os.path.exists(repo_dir):
+        raise HTTPException(status_code=404, detail="Repository not found")
+
+    cmd = f"git -C {repo_dir} log --all --pretty=format:'%an <%ae>' | awk '/<{email}>/ {{print; exit}}'"
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+
+    if process.returncode != 0:
+        error_message = stderr.decode().strip()
+        logging.error(f"Error while executing command: {cmd}. Error: {error_message}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    log_output = stdout.decode().strip()
+    if log_output:
+        name = log_output.split(" <")[0]
+        return {"email": email, "name": name}
+
+    raise HTTPException(status_code=404, detail="User not found")
