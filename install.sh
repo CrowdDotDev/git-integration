d067daa9d3668b6aadf8c2fe6d009708c5a6ea43
Mish Savelyev <michael.savelyev@gmail.com>
2024-08-09 15:48:34 +0000

    Fix some things in the install.sh script

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
 sudo apt update
-sudo apt upgrade
-sudo apt install python3-pip
-sudo apt install python-is-python3
-sudo apt install python3.10-venv
-sudo mkdir /data
+sudo apt upgrade -y
+sudo apt install -y python3-pip
+sudo apt install -y python-is-python3
+sudo apt install -y python3-venv
+sudo mkdir -p /data
 sudo chown ubuntu /data
 mkdir -p /data/repos/log
 mkdir -p ~/venv/cgit && python -m venv ~/venv/cgit
 source ~/venv/cgit/bin/activate
 pip install --upgrade pip
-cd ~/git-integration ; pip install -e .
+cd ~/git-integration
+pip install -e .
 
 # Move one directory up from the git-integration folder
 cd ~
 
 # Check if git-integration-environment folder exists
-if [ ! -d "git-integration-environment" ]; then 
+if [ ! -d "git-integration-environment" ]; then
   git clone git@github.com:CrowdDotDev/git-integration-environment.git
 else
   # Pull the latest changes from git-integration-environment repository if it exists
@@ -27,8 +32,7 @@ fi
 # Move back into the git-integration folder
 cd ~
 
-if [ "$1" == "prod" ]
-then
+if [ "$1" == "prod" ]; then
   # Copy the dotenv-prod file as .env, replacing it if it already exists
   cp -f ~/git-integration-environment/dotenv-prod .env
   cp -f ~/git-integration-environment/dotenv-prod git-integration/.env
@@ -36,7 +40,7 @@ then
 else
   # Copy the dotenv-staging file as .env, replacing it if it already exists
   cp -f ~/git-integration-environment/dotenv-staging .env
-  cp -f ~/git-integration-environment/dotenv-staging git-integration.env
+  cp -f ~/git-integration-environment/dotenv-staging git-integration/env
   echo "The dotenv-staging file has been copied as .env"
 fi
 
@@ -46,19 +50,19 @@ touch tmp-cron
 
 # Check if the user has a crontab
 if crontab -l >/dev/null 2>&1; then
-    # If the cron job already exists, skip adding
-    if crontab -l | grep -q "/home/ubuntu/venv/cgit/bin/crowd-git-ingest"; then
-        echo "Cron job already exists. Nothing to do."
-        rm tmp-cron
-        exit 0
-    fi
+  # If the cron job already exists, skip adding
+  if crontab -l | grep -q "/home/ubuntu/venv/cgit/bin/crowd-git-ingest"; then
+    echo "Cron job already exists. Nothing to do."
+    rm tmp-cron
+    exit 0
+  fi
 
-    # Save the current crontab into a temporary file
-    crontab -l > tmp-cron
+  # Save the current crontab into a temporary file
+  crontab -l >tmp-cron
 fi
 
 # Append the new cron job entry
-echo "0 * * * * /home/ubuntu/venv/cgit/bin/crowd-git-ingest >> /data/repos/log/cron.log 2>&1" >> tmp-cron
+echo "0 * * * * /home/ubuntu/venv/cgit/bin/crowd-git-ingest >> /data/repos/log/cron.log 2>&1" >>tmp-cron
 
 # Install the new crontab
 crontab tmp-cron
